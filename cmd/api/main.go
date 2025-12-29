00dd033af0210fb4926be446f56eac812d665ce7
gallfeder <finnick4gallfeder@gmail.com>
2025-12-29 13:45:10 +0100

    [MAIN] Added .env variable reading. This so far supports LOG_LEVEL, ADDRESS and PORT

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,17 +2,50 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"net/http"
+	"os"
 	"stockviewer/internal/handlers"
 	"stockviewer/internal/stocks"
 	"time"
+
+	"github.com/joho/godotenv"
 
 	"github.com/go-chi/chi"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	//log.SetLevel(log.DebugLevel)
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Error("No .env file found!")
+	}
+
+	log_level, ok := os.LookupEnv("LOG_LEVEL")
+	if !ok {
+		log.Warn("LOG_LEVEL is not set in .env! Using default value.")
+		log_level = "Info"
+	}
+	loglvl, err := log.ParseLevel(log_level)
+
+	if err != nil {
+		log.Warnf("LOG_LEVEL as defined in .env is not a valid log level!")
+	} else {
+		log.SetLevel(loglvl)
+	}
+
+	address, ok := os.LookupEnv("ADDRESS")
+	if !ok {
+		log.Warn("ADDRESS is not set in .env! Using default value.")
+		address = "localhost"
+	}
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		log.Warn("PORT is not set in .env! Using default value.")
+		port = "8000"
+	}
+
 	log.SetReportCaller(true)
 	var r *chi.Mux = chi.NewRouter()
 	handlers.Handler(r)
@@ -60,7 +93,7 @@ func main() {
 
 	log.Info("Starting GO API service...")
 
-	err = http.ListenAndServe("localhost:8000", r)
+	err = http.ListenAndServe(fmt.Sprintf("%v:%v", address, port), r)
 
 	if err != nil {
 		log.Fatal(err)
