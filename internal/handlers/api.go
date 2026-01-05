58d4c129f6ef647dcf4deff5a6bd4f6c6ca27977
gallfeder <finnick4gallfeder@gmail.com>
2026-01-05 20:23:48 +0100

    [FRONTEND] Added routing to static site (currently a copy of the api documentation as a demo)

--- internal/handlers/api.go
+++ internal/handlers/api.go
@@ -1,12 +1,17 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/go-chi/chi"
 	chimiddle "github.com/go-chi/chi/middleware"
 )
 
 func Handler(r *chi.Mux) {
 	r.Use(chimiddle.StripSlashes)
+
+	fileserver := http.FileServer(http.Dir("./static"))
+	r.Handle("/*", fileserver)
 
 	r.Route("/api/stocks", func(router chi.Router) {
 
