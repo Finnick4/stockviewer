84e7addde8263fea0ae9171758d2d7ea6d479b5f
gallfeder <finnick4gallfeder@gmail.com>
2026-01-06 22:04:32 +0100

    [STOCKLIST] Added fetching the API for the stocknames (for now...)

--- static/components/stocklist.js
+++ static/components/stocklist.js
@@ -1,43 +1,37 @@
 class stocklistAll extends HTMLElement {
     connectedCallback() {
+        try {
+            fetch(`${window.location.origin}/api/stocks`).then(resp => resp.json()).then(obj => {
+                let html = ""
+                obj["Data"].forEach(e => {
+                    html += `<div class="stockoverview">
+                            <div class="stockname">${e["Name"]}</div>
+                            <div>
+                                <div class="change positive">200.63m</div>
+                                <div class="change positive">+10</div>
+                                <div class="change positive">+1%</div>
+                            </div>
+                        </div>`
+                })
+
+                this.innerHTML = `<div class="stocklist">
+                        <h2>All stocks</h2>
+                        ${html}
+                    </div>
+                `
+            })
+        } catch (e) {
+            console.log(e)
+            this.innerHTML = `<div class="stocklist">
+                    <h2>All stocks</h2>
+                    <p>Failed to get stocks!</p>
+                </div>`
+        }
+
         this.innerHTML = `<div class="stocklist">
-        <h2>All stocks</h2>
-        <div class="stockoverview">
-            <div class="stockname">Lorem Ipsum</div>
-            <div>
-                <div class="change positive">+10</div>
-                <div class="change positive">+1%</div>
-            </div>
-        </div>
-        <div class="stockoverview">
-            <div class="stockname">Lorem Ipsum</div>
-            <div>
-                <div class="change positive">+100</div>
-                <div class="change positive">+0.1%</div>
-            </div>
-        </div>
-        <div class="stockoverview">
-            <div class="stockname">Lorem Ipsum</div>
-            <div>
-                <div class="change negative">-100</div>
-                <div class="change negative">-0.1%</div>
-            </div>
-        </div>
-        <div class="stockoverview">
-            <div class="stockname">WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW</div>
-            <div>
-                <div class="change negative">-100</div>
-                <div class="change negative">-0.1%</div>
-            </div>
-        </div>
-        <div class="stockoverview">
-            <div class="stockname">Senatus Populusque Romanum</div>
-            <div>
-                <div class="change negative">-100</div>
-                <div class="change negative">-0.1%</div>
-            </div>
-        </div>
-    </div>`
+                <h2>All stocks</h2>
+                <p>Loading stock data...</p>
+            </div>`
     }
 }
 
