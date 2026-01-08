37ad473fa986565daffca779a062425c71a6ae3d
gallfeder <finnick4gallfeder@gmail.com>
2026-01-08 21:30:01 +0100

    [Stocklist] Added last change, change percentage and color to indicate if it is positive or negative to the stock list

--- static/components/stocklist.js
+++ static/components/stocklist.js
@@ -13,7 +13,7 @@ class stocklistAll extends HTMLElement {
                             <div class="stockname">${e["Name"]}</div>
                             <div class="${e["DeltaAmount"] >= 0 ? "positive" : "negative"}">
                                 <div class="change">${shortPrice}</div>
-                                <div class="change">${e["DeltaAmount"] / 100}</div>
+                                <div class="change">${e["DeltaAmount"] / 100}€</div>
                                 <div class="change">${(e["Price2"]/e["Price1"] - 1.0).toFixed(2)}%</div>
                             </div>
                         </div>`
