a0a66813420b5dd3bc4cc915b9d29b7b9c59c6a0
gallfeder <finnick4gallfeder@gmail.com>
2026-01-15 18:15:38 +0100

    [API] Moved code to return the data into their own functions for easier readability

--- internal/handlers/stocks/get_stocks.go
+++ internal/handlers/stocks/get_stocks.go
@@ -16,7 +16,7 @@ import (
 
 func GetStocks(w http.ResponseWriter, r *http.Request) {
 	t := time.Now()
-	log.Debugf("Inquiring all stocks")
+	log.Debugf("Inquiring stocks")
 	var params = api.StockGetParams{}
 	var decoder *schema.Decoder = schema.NewDecoder()
 	var err error
@@ -32,52 +32,54 @@ func GetStocks(w http.ResponseWriter, r *http.Request) {
 	// TODO: Return history if a specific stock is requested
 
 	if database.IsValidTimeframeScope(params.Timeframe) {
-		log.Debugf("Getting stocks and deltas in timeframe %v", params.Timeframe)
-		deltas, err := database.GetStocksPriceDelta()
+		err = sendAllStockDeltas(w, params.Timeframe)
 		if err != nil {
 			log.Error(err)
 			api.InternalErrorHandler(w)
 			return
 		}
-
-		var response = api.StockGetDeltasResponse{
-			Code: http.StatusOK,
-			Data: deltas,
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		err = json.NewEncoder(w).Encode(response)
-		if err != nil {
-			log.Error(err)
-			api.InternalErrorHandler(w)
-			return
-		}
-		log.Debugf("Time took to get all stocks with price delta was %v", time.Since(t))
-		return
-
+		log.Debugf("Time took to get all stocks with deltas is %v", time.Since(t))
 	} else {
-		log.Debug("Getting all current stock data")
-		data, err := database.GetCurrentStockInformation()
-
+		err = sendAllStocks(w)
 		if err != nil {
 			log.Error(err)
 			api.InternalErrorHandler(w)
 			return
 		}
-
-		var response = api.StockGetResponse{
-			Code: http.StatusOK,
-			Data: data,
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		err = json.NewEncoder(w).Encode(response)
-		if err != nil {
-			log.Error(err)
-			api.InternalErrorHandler(w)
-			return
-		}
-		log.Debugf("Time took to get all stocks was %v", time.Since(t))
-		return
+		log.Debugf("Time took to get all stocks is %v", time.Since(t))
 	}
+
+}
+
+func sendAllStockDeltas(w http.ResponseWriter, timeframeScope int64) error {
+	log.Debugf("Getting stocks and deltas in timeframe %v", timeframeScope)
+	deltas, err := database.GetStocksPriceDelta()
+	if err != nil {
+		return err
+	}
+
+	var response = api.StockGetDeltasResponse{
+		Code: http.StatusOK,
+		Data: deltas,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(response)
+}
+
+func sendAllStocks(w http.ResponseWriter) error {
+	log.Debug("Getting all current stock data")
+	data, err := database.GetCurrentStockInformation()
+
+	if err != nil {
+		return err
+	}
+
+	var response = api.StockGetResponse{
+		Code: http.StatusOK,
+		Data: data,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(response)
 }
