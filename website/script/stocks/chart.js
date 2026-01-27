078cc04eb5e224f9154ba826839f493df88a766b
gallfeder <finnick4gallfeder@gmail.com>
2026-01-27 20:32:32 +0100

    [F/STOCKS] Reiterated on the stock graph by adding axis

--- website/script/stocks/chart.js
+++ website/script/stocks/chart.js
@@ -5,8 +5,10 @@ class stockChart extends HTMLElement {
                         <div class="inner">
                             <h2>${this.stockid}</h2>
                             <nav class="timeframe-selector"></nav>
-                            <svg class="chart" viewbox="0 0 100 100" width="10rem">
-                                <path d="M0 0 L1 1 L10 1 L40 50 L80 10" ></path>
+                            <svg class="chart" viewbox="0 0 100 50">
+                                <path d="M10 25 L20 10 L30 30 L40 40 L50 20" ></path>
+                                <line class="axis" x1="10" x2="10" y1="5" y2="45"></line>
+                                <line class="axis" x1="10" x2="90" y1="45" y2="45"></line>
                             </svg>
                         </div>
                         `
