0cc11c9ece24225f6984ae9b4adb8e05118fd36c
gallfeder <finnick4gallfeder@gmail.com>
2026-01-31 16:49:42 +0100

    [B/DATA] Also subscribed stocklist to the correct data.

--- website/script/stocks/stocklist.js
+++ website/script/stocks/stocklist.js
@@ -1,43 +1,39 @@
 class stocklistAll extends HTMLElement {
     connectedCallback() {
-        try {
-            fetch(`${window.location.origin}/api/stocks/?Timeframe=1`).then(resp => resp.json()).then(obj => {
-                let html = ""
-                obj["Data"].forEach(e => {
-                    let shortPrice = (e["Price2"]/100).toLocaleString('en-US', {
-                        maximumFractionDigits: 2,
-                        notation: 'compact',
-                        compactDisplay: 'short'
-                    });
-                    html += `<a class="stockoverview" is="a-button" data-stock-id="${e["ID"]}" href="${window.location.origin}/stocks/${e["ID"]}">
-                            <div class="stockname">${e["Name"]}</div>
+        this.timeframe = 1
+        this.innerHTML = `<div class="stocklist">
+                <h2>All stocks</h2>
+                <p>Loading stock data...</p>
+            </div>`
+        subscribeToAPI(`/api/stocks/?Timeframe=${this.timeframe}`, addThisToFunctionCall(this.updateData, this))
+    }
+
+    updateData(data, that) {
+        let html = ""
+        data.forEach(e => {
+            let shortPrice = (e["Price2"]/100).toLocaleString('en-US', {
+                maximumFractionDigits: 2,
+                notation: 'compact',
+                compactDisplay: 'short'
+            });
+            html += `<div class="stockoverview"  data-stock-id="${e["ID"]}">
+                            <a is="a-button" class="stockname" href="${window.location.origin}/stocks/${e["ID"]}">${e["Name"]}</a>
                             <div class="${e["DeltaAmount"] >= 0 ? "positive" : "negative"}">
                                 <div class="change">${shortPrice}</div>
                                 <div class="change">${e["DeltaAmount"] / 100}€</div>
                                 <div class="change">${(e["Price2"]/e["Price1"] - 1.0).toFixed(2)}%</div>
                             </div>
-                        </a>`
-                })
+                        </div>`
+        })
 
-                this.innerHTML = `<div class="stocklist">
+        that.innerHTML = `<div class="stocklist">
                         <h2>All stocks</h2>
                         ${html}
                     </div>
                 `
-            })
-        } catch (e) {
-            console.log(e)
-            this.innerHTML = `<div class="stocklist">
-                    <h2>All stocks</h2>
-                    <p>Failed to get stocks!</p>
-                </div>`
-        }
-
-        this.innerHTML = `<div class="stocklist">
-                <h2>All stocks</h2>
-                <p>Loading stock data...</p>
-            </div>`
     }
 }
+
+
 
 customElements.define('stocklist-all', stocklistAll);
