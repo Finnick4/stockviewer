340cc215be843fda7e25ebcc0b9c89153df36759
gallfeder <finnick4gallfeder@gmail.com>
2026-02-08 13:27:40 +0100

    [F/DATA] Added getting user information like username and permissions. Also added the username to the user manager element

--- website/script/headerbar/user_manager.js
+++ website/script/headerbar/user_manager.js
@@ -4,10 +4,12 @@ class userManagerElement extends HTMLElement {
         this.dropdownid = createDropdown(`This is a test!!!`)
         this.innerHTML = `
         <button popovertarget="${this.dropdownid}" class="usermanager" style="anchor-name: --anchor-${this.dropdownid};">
-            <div class="name">USER</div>
+            <div class="name">loading...</div>
             <img class="icon" src="/icons/user.svg" alt="user icon" draggable="false">
         </button>`
-
+        userInformation.writeName(name => {
+            this.querySelector("div.name").innerHTML = name
+        })
     }
     disconnectedCallback() {
         deleteDropdown(this.dropdownid)
