aa454dd5afb24aa3c1a2c756c70bfc8aa9880b0f
gallfeder <finnick4gallfeder@gmail.com>
2026-02-09 17:10:13 +0100

    [F/DATA] Changed userInformation to be an object only having functions to utilise the stored data. Said data should be private.

--- website/script/datacollector/user_information.js
+++ website/script/datacollector/user_information.js
@@ -1,35 +1,33 @@
 /**
  * Stores everything to know about the user that is logged in.
  */
-let userInformation = {
-    name: "",
-    permissions: {},
-    update() {
-        checkLoggedIn()
-        fetch(window.location.origin + "/api/users/permissions").then(resp => resp.json()).then(jsonResponse => {
-            if (jsonResponse["Data"] === null || jsonResponse["Code"] !== 200) {
-                userInformation.permissions = {}
-                return
+let userInformation = (function (){
+    let name = "";
+    let permissions = {};
+
+    checkLoggedIn()
+    fetch(window.location.origin + "/api/users/permissions").then(resp => resp.json()).then(jsonResponse => {
+        if (jsonResponse["Data"] === null || jsonResponse["Code"] !== 200) {
+            userInformation.permissions = {}
+            return
+        }
+        jsonResponse["Data"].forEach(perm => permissions[perm["Permission"]] = perm["Value"])
+    })
+    fetch(window.location.origin + "/api/users/overview").then(resp => resp.json()).then(jsonResponse => {this.name = jsonResponse["Data"]})
+
+    return {
+        writeName(fn) {
+            if (name === "") {
+                fetch(window.location.origin + "/api/users/overview").then(resp => resp.json()).then(jsonResponse => {
+                    name = jsonResponse["Data"]
+                    console.log(name)
+                    fn(name)
+                })
+            } else {
+                console.log(name)
+                fn(name)
             }
-            jsonResponse["Data"].forEach(perm => userInformation.permissions[perm["Permission"]] = perm["Value"])
-        })
-        fetch(window.location.origin + "/api/users/overview").then(resp => resp.json()).then(jsonResponse => {this.name = jsonResponse["Data"]})
-    },
-    writeName(fn) {
-        if (this.name === "") {
-            fetch(window.location.origin + "/api/users/overview").then(resp => resp.json()).then(jsonResponse => {
-                this.name = jsonResponse["Data"]
-                console.log(this.name)
-                fn(this.name)
-            })
-        } else {
-            console.log(this.name)
-            fn(this.name)
         }
     }
-}
-
-
-userInformation.update();
-
+})()
 
