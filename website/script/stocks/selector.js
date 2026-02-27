8a71966d4419a28bd6131228dc648e4e78556172
gallfeder <finnick4gallfeder@gmail.com>
2026-02-27 17:13:28 +0100

    [F/STOCKS] Added search functionality to stock selector (currently ONLY searches)

--- website/script/stocks/selector.js
+++ website/script/stocks/selector.js
@@ -8,22 +8,43 @@ class stockSelectorElement extends HTMLElement {
                 </div>
                 <li>Loading stocks...</li>
             </ul>`
+
+        this.dropdownid = createDropdown(`Please type to search...`)
+
         this.search = this.querySelector("input.search")
-        this.search.addEventListener("input", e => {
-            console.log(this.search.value)
+        this.dropdown = document.getElementById(this.dropdownid)
+
+        this.search.popovertarget = this.dropdownid
+        this.search.style.anchorName = `--anchor-${this.dropdownid}`
+        this.search.addEventListener("click", () => {
+            this.dropdown.togglePopover()
         })
+
+        this.dropdown.style.width = "calc(32ch -  1rem)"
+
         fetch("/api/stocks").then(r => r.json()).then(resp => {
             const data = resp["Data"]
 
             this.search.addEventListener("input", e => {
                 let possible = []
-                data.forEach(stock =>  {
-                    if (stock["Name"].toLowerCase().startsWith(e.target.value.toLowerCase())) {
+                for (const stock of data) {
+                    if (possible.length >= 5) {
+                        break
+                    }
+                    if (stock["Name"].toLowerCase().includes(e.target.value.toLowerCase())) {
                         possible.push(stock)
                     }
-                })
+                }
                 console.log(e.target.value)
                 console.log(possible)
+                let html = ""
+                possible.forEach(stock => {
+                    html += `<button class="searchResult" data-stock-id="${stock["ID"]}">${sanitiseText(stock["Name"])}</button>`
+                })
+                if (possible.length === 0) {
+                    html = "No stocks found"
+                }
+                this.dropdown.innerHTML = html
             })
         })
 
