b36a2d9d227d7b1e03fff1ee221d46aba2ca82d8
gallfeder <finnick4gallfeder@gmail.com>
2026-03-27 18:48:28 +0100

    [F/STOCKS] Added showing articles affecting a stock

---
+++ website/script/stocks/influences_list.js
@@ -0,0 +1,42 @@
+class stockInfluenceList extends HTMLElement {
+    connectedCallback() {
+        this.stockid = Number(this.getAttribute("data-stock-id"))
+
+        this.innerHTML = `<div class="inner">
+                <nav><h2>Influences</h2></nav>
+                <p>Loading influences on stock...</p>
+            </div>`
+        fetch(`/api/stocks/${this.stockid}/influences`).then(r => r.json()).then(resp => {
+            const data = resp["Data"]
+            if (data === null || data.length === 0) {
+                this.innerHTML = `<div class="inner">
+                <nav><h2>Influences</h2></nav>
+                <p>This stock currently is not influenced by any article...</p>
+            </div>`
+            }
+
+            let html = ""
+            data.forEach(article => {
+                html += `<li class="articlePreview" data-article-id="${article["ArticleID"]}">
+                            <a is="a-button" href="/articles/${article["ArticleID"]}">
+                                <div class="title">${sanitiseText(article["ArticleTitle"])}</div>
+                                <div class="info ${article["PermillePerDay"] >= 0 ? "positive" : "negative"}">
+                                    <div class="change">${article["PermillePerDay"]}&permil;/day</div>
+                                    <div class="change">${article["LengthMinutes"]}m</div>
+                                </div>
+                            </a>
+                        </li>`
+            })
+
+            this.innerHTML = `<ul class="inner">
+                        <nav><h2>Influences</h2></nav>
+                        ${html}
+                    </ul>
+                `
+        })
+    }
+}
+
+
+
+customElements.define('stock-influence-list', stockInfluenceList);
