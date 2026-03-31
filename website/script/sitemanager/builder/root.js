d3d0b8536601ffad78ebd03453f3a8d1f3ecdb8b
gallfeder <finnick4gallfeder@gmail.com>
2026-03-31 21:17:53 +0200

    [F/STOCKGROUPS] Added viewing all starred stock groups

--- website/script/sitemanager/builder/root.js
+++ website/script/sitemanager/builder/root.js
@@ -1,6 +1,7 @@
 function buildRootPage() {
     const main = `
         <stockgroups-members-list class="starredStocksList" data-stock-group-id="-1" data-alt-title="Starred Stocks"></stockgroups-members-list>
+        <stockgroups-starred-list></stockgroups-starred-list>
         <relevant-articles class="articleInbox"></relevant-articles>
         `;
     setMainBodyHTML(main, "homepage");
---
+++ website/script/stockgroups/stockgroups_starred_list.js
@@ -0,0 +1,49 @@
+class stockgroupsStarredList extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `<div class="inner">
+                <nav><h2>${getTranslatedStr("stockgroups.starred_list.title")}</h2></nav>
+                <p>${getTranslatedStr("stockgroups.starred_list.loading")}</p>
+            </div>`
+        this.closeSubscription = subscribeToAPI(`/api/stockgroups/starred/sse`, addThisToFunctionCall(this.updateData, this))
+    }
+
+    disconnectedCallback() {
+        this.closeSubscription()
+    }
+
+    updateData(data, that) {
+        if (data === null) {
+            that.innerHTML = `<div class="inner">
+                <nav><h2>${getTranslatedStr("stockgroups.starred_list.title")}</h2></nav>
+                <p>${getTranslatedStr("stockgroups.starred_list.none")}</p>
+            </div>`
+            return
+        }
+        let html = ""
+        data.forEach(e => {
+            html += `<li class="stockOverview"  data-stock-group-id="${e["ID"]}">
+                            <a class="containing" is="a-button" href="/groups/${e["ID"]}">
+                                <div class="stockName">${sanitiseText(e["Name"])}</div>
+                                <div class="info">
+                                    <div class="change">${getShortNumber(e["TotalValue"]/100)}</div>
+                                    <div class="change">${getTranslatedStr("stockgroups.member_count", {num: e["MemberCount"]})}</div>
+                                </div>
+                            </a>
+                        </li>`
+        })
+
+        that.innerHTML = `<ul class="inner">
+                        <div class="titlebar">
+                            <nav></nav>
+                            <h2>${getTranslatedStr("stockgroups.starred_list.title")}</h2>
+                            <div></div>
+                        </div>
+                        ${html}
+                    </ul>
+                `
+    }
+}
+
+
+
+customElements.define('stockgroups-starred-list', stockgroupsStarredList);
