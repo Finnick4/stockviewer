bb6416d59dce88a29c198b0f2e4a236b277580bc
gallfeder <finnick4gallfeder@gmail.com>
2026-04-23 21:10:31 +0200

    [FRONTEND] Fixed buttons of the user manager reloading the site

--- website/script/headerbar/user_manager.js
+++ website/script/headerbar/user_manager.js
@@ -2,8 +2,8 @@
 class userManagerElement extends HTMLElement {
     connectedCallback() {
         this.dropdownid = createDropdown(`
-        <a is="a_link" class="btn" href="/settings">${getTranslatedStr("user_manager.dashboard_account")}</a>
-        <a is="a_link" class="btn" href="/admin">${getTranslatedStr("user_manager.dashboard_admin")}</a>
+        <a is="a-button" class="btn" href="/settings">${getTranslatedStr("user_manager.dashboard_account")}</a>
+        <a is="a-button" class="btn" href="/admin">${getTranslatedStr("user_manager.dashboard_admin")}</a>
         <button onclick="logout()">${getTranslatedStr("user_manager.logout")}</button>
         `)
         this.innerHTML = `
