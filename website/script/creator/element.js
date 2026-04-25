6c5a7a0e3b49d77b2b40593a400e3d1767a937b4
gallfeder <finnick4gallfeder@gmail.com>
2026-04-25 20:09:01 +0200

    [F/CREATOR] Hid options which one isn't permitted to in creator dropdown

--- website/script/creator/element.js
+++ website/script/creator/element.js
@@ -1,10 +1,13 @@
 class createDataElement extends HTMLElement {
     connectedCallback() {
-        this.dropdownid = createDropdown(`<button onclick="showModalCreateStock(this)">${getTranslatedStr("creator.stock")}</button>
-                                                <button onclick="showModalCreateStockGroup(this)">${getTranslatedStr("creator.stock_group")}</button>
-                                                <button onclick="showModalCreateArticle(this)">${getTranslatedStr("creator.article")}</button>
-                                                <button onclick="showModalCreateUser(this)">${getTranslatedStr("creator.user")}</button>
-                                            `)
+        let html = ``
+
+        if (userInfo.checkPerm("canCreateStocks")) html += `<button onclick="showModalCreateStock(this)">${getTranslatedStr("creator.stock")}</button>`
+        if (userInfo.checkPerm("canCreateStockGroups")) html += `<button onclick="showModalCreateStockGroup(this)">${getTranslatedStr("creator.stock_group")}</button>`
+        if (userInfo.checkPerm("canCreateArticles")) html += `<button onclick="showModalCreateArticle(this)">${getTranslatedStr("creator.article")}</button>`
+        if (userInfo.checkPerm("canCreateUsers")) html += `<button onclick="showModalCreateUser(this)">${getTranslatedStr("creator.user")}</button>`
+
+        this.dropdownid = createDropdown(html)
         this.innerHTML = `<button popovertarget="${this.dropdownid}" style="anchor-name: --anchor-${this.dropdownid};"><img class="icon" src="/icons/plussign.svg" alt="create new" draggable="false"></button>`
     }
     disconnectedCallback() {
