22aa12274c4cb03b4532918f47d9505d8d31a282
gallfeder <finnick4gallfeder@gmail.com>
2026-04-29 22:45:27 +0200

    [B/ARTICLES] Added deleting an article via the API

---
+++ internal/handlers/articles/delete_article.go
@@ -0,0 +1,51 @@
+package articles
+
+import (
+	"encoding/json"
+	"net/http"
+	"stockviewer/api"
+	"stockviewer/internal/database"
+	"strconv"
+
+	"github.com/go-chi/chi"
+	log "github.com/sirupsen/logrus"
+)
+
+func DeleteArticle(w http.ResponseWriter, r *http.Request) {
+	articleID, err := strconv.Atoi(chi.URLParam(r, "articleID"))
+
+	if err != nil || articleID <= 0 {
+		api.RequestMalformedHandler(w, "Could not parse article ID!")
+		return
+	}
+
+	permissions := r.Context().Value("permissions").(map[string]int32)
+
+	if permissions["canDeleteArticles"] != 1 {
+		api.InsufficientPermissionHandler(w)
+		return
+	}
+
+	log.Debugf("Trying to delete article %v", articleID)
+
+	err = database.DeleteArticle(int32(articleID))
+
+	if err != nil {
+		api.InternalErrorHandler(w)
+		log.Error(err)
+		return
+	}
+
+	var response = api.SuccessResponse{
+		Code: http.StatusOK,
+		Data: "success",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		log.Error(err)
+		api.InternalErrorHandler(w)
+		return
+	}
+}
