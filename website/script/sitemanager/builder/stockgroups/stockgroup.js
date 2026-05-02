36feacea6ce830bee2900f335e06ca0e4084273b
gallfeder <finnick4gallfeder@gmail.com>
2026-05-02 22:07:04 +0200

    [F/STOCKGROUPS] Changed layout of stock groups sites

--- website/script/sitemanager/builder/stockgroups/stockgroup.js
+++ website/script/sitemanager/builder/stockgroups/stockgroup.js
@@ -8,8 +8,9 @@ function buildIndividualStockGroupPage(id) {
                         <stock-group-header data-stock-group-id="${id}"></stock-group-header>
                         <stockgroups-member-pie-chart data-stock-group-id="${id}"></stockgroups-member-pie-chart>
                         <stockgroups-members-list data-stock-group-id="${id}"></stockgroups-members-list>
+                        <stock-group-chart data-stock-group-id="${id}"></stock-group-chart>
                         <stock-group-description data-stock-group-id="${id}"></stock-group-description>
                         <stockgroup-influence-list data-stock-group-id="${id}"></stockgroup-influence-list>
-                        <stock-group-chart data-stock-group-id="${id}"></stock-group-chart>`;
+`;
     setMainBodyHTML(main, "stockGroupOverviewPage");
 }
