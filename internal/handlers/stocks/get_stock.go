66cb9ee03eb477f62da502257789ec02bb72ad92
gallfeder <finnick4gallfeder@gmail.com>
2026-05-07 21:24:01 +0200

    [B/STOCKS] Fixed issue with getting a stock resulting in baring getting histories

--- internal/handlers/stocks/get_stock.go
+++ internal/handlers/stocks/get_stock.go
@@ -39,58 +39,52 @@ func GetStock(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var send func()
-
 	if dto.IsValidTimeframeLength(params.Timeframe) {
-		send = func() {
-			history, err := database.GetStockPriceHistory(int32(stockID), dto.GenerateTimeframe(params.Timeframe))
-			if err != nil {
-				log.Error(err)
-				api.InternalErrorHandler(w)
-				return
-			}
+		history, err := database.GetStockPriceHistory(int32(stockID), dto.GenerateTimeframe(params.Timeframe))
+		if err != nil {
+			log.Error(err)
+			api.InternalErrorHandler(w)
+			return
+		}
 
-			if len(history) == 0 {
-				log.Error(err)
-				api.RequestNothingFoundHandler(w, "Did not find a stock with the given ID.")
-				return
-			}
+		if len(history) == 0 {
+			log.Error(err)
+			api.RequestNothingFoundHandler(w, "Did not find a stock with the given ID.")
+			return
+		}
 
-			var response = api.SuccessResponse{
-				Code: http.StatusOK,
-				Data: history,
-			}
+		var response = api.SuccessResponse{
+			Code: http.StatusOK,
+			Data: history,
+		}
 
-			w.Header().Set("Content-Type", "application/json")
-			err = json.NewEncoder(w).Encode(response)
-			if err != nil {
-				log.Error(err)
-				api.InternalErrorHandler(w)
-				return
-			}
+		w.Header().Set("Content-Type", "application/json")
+		err = json.NewEncoder(w).Encode(response)
+		if err != nil {
+			log.Error(err)
+			api.InternalErrorHandler(w)
+			return
 		}
 
 	} else {
-		send = func() {
-			price, err := database.GetStockInfo(int32(stockID), userID)
-			if err != nil {
-				log.Error(err)
-				return
-			}
-
-			var response = api.SuccessResponse{
-				Code: http.StatusOK,
-				Data: price,
-			}
-
-			w.Header().Set("Content-Type", "application/json")
-			err = json.NewEncoder(w).Encode(response)
-			if err != nil {
-				log.Error(err)
-				api.InternalErrorHandler(w)
-				return
-			}
+		price, err := database.GetStockInfo(int32(stockID), userID)
+		if err != nil {
+			log.Error(err)
+			return
 		}
+
+		var response = api.SuccessResponse{
+			Code: http.StatusOK,
+			Data: price,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		err = json.NewEncoder(w).Encode(response)
+		if err != nil {
+			log.Error(err)
+			api.InternalErrorHandler(w)
+			return
+		}
+
 	}
-	send()
 }
