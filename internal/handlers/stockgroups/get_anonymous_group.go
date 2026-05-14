3fd4cbe650b68687935832679039a357e36a59f0
gallfeder <finnick4gallfeder@gmail.com>
2026-05-14 12:04:48 +0200

    [B/STOCKGROUPS] Added compliance to empty anonymous stock groups

--- internal/handlers/stockgroups/get_anonymous_group.go
+++ internal/handlers/stockgroups/get_anonymous_group.go
@@ -26,6 +26,30 @@ func GetStockAnonymousGroup(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if len(params.Members) == 0 {
+		log.Debug("There are no members for the anonymous stock group!")
+		var data any
+		if dto.IsValidTimeframeLength(params.Timeframe) {
+			data = nil
+		} else {
+			data = dto.DetailedStockGroup{}
+		}
+
+		var response = api.SuccessResponse{
+			Code: http.StatusOK,
+			Data: data,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		err = json.NewEncoder(w).Encode(response)
+		if err != nil {
+			api.InternalErrorHandler(w)
+			log.Debug(err)
+			return
+		}
+		return
+	}
+
 	if !database.AreActiveStockIDs(params.Members) {
 		log.Debug("One of the stocks to be queried is invalid.")
 		api.RequestMalformedHandler(w, "One of the stocks to be queried is invalid.")
