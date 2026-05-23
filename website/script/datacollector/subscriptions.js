9c6b606cb8308fa077357335bdc433a021983b85
gallfeder <finnick4gallfeder@gmail.com>
2026-05-23 12:28:03 +0200

    [F/DATA] Added freezing SSE connections when open tab changes.
    This allows one to switch between multiple Stock Viewer tabs without having a bottleneck due to max SSE connections

--- website/script/datacollector/subscriptions.js
+++ website/script/datacollector/subscriptions.js
@@ -2,6 +2,7 @@
 let currentSubscriptions = {}
 let subscriptionListeners = {}
 let currentSubscriptionCount = 0
+let frozenSubscriptions = {}
 
 function subscribeToAPI(path, func) {
     if (typeof path !== "string" || typeof func !== "function") {
@@ -46,13 +47,18 @@ function subscribeToAPI(path, func) {
     }
 
     return () => {
-        currentSubscriptions[path] = currentSubscriptions[path].filter(sub => (sub["id"] !== id))
+        currentSubscriptions[path] = currentSubscriptions[path]?.filter(sub => (sub["id"] !== id))
+        frozenSubscriptions[path] = frozenSubscriptions[path]?.filter(sub => (sub["id"] !== id))
 
-        if (currentSubscriptions[path].length === 0) {
+        if (currentSubscriptions[path]?.length === 0) {
             console.log("closing " + path)
             delete currentSubscriptions[path]
             subscriptionListeners[path].es.close()
             delete subscriptionListeners[path]
+        }
+        if (frozenSubscriptions[path]?.length === 0) {
+            console.log("closing (frozen) " + path)
+            delete frozenSubscriptions[path]
         }
     }
 }
@@ -63,6 +69,12 @@ function closeUnneededSubscriptions() {
         if (currentSubscriptions[path].length === 0) {
             console.log("closing " + path)
             delete currentSubscriptions[path]
+            subscriptionListeners[path].es.close()
+            delete subscriptionListeners[path]
+        }
+    })
+    Object.keys(subscriptionListeners).forEach(path => {
+        if (Object.keys(currentSubscriptions).indexOf(path) === - 1) {
             subscriptionListeners[path].es.close()
             delete subscriptionListeners[path]
         }
@@ -80,3 +92,70 @@ async function pingDataSubscribed(path) {
         })
     }
 }
+
+
+document.addEventListener("visibilitychange", () => {
+    if (document.hidden) {
+        if (currentSubscriptions.length === 0) {
+            return
+        }
+        console.log("Freezing subscriptions")
+
+        for (const path of Object.keys(currentSubscriptions)) {
+            const subscriptionGroup = currentSubscriptions[path]
+            if (Object.keys(frozenSubscriptions).indexOf(path) !== - 1) {
+                for (const subscription of subscriptionGroup) {
+                    frozenSubscriptions[path].add(subscription)
+                }
+            } else {
+                frozenSubscriptions[path] = subscriptionGroup
+            }
+        }
+        currentSubscriptions = {}
+
+        closeUnneededSubscriptions()
+    } else {
+        if (frozenSubscriptions.length === 0) {
+            return
+        }
+
+        const addSSEListener = path => {
+            console.log(`Readding SSE Connection for ${path}`)
+            subscriptionListeners[path] = {
+                es: undefined,
+                cache: []
+            }
+
+            subscriptionListeners[path].es = new EventSource(window.location.origin + path);
+
+            subscriptionListeners[path].es.addEventListener("stockupdate", event => {
+                subscriptionListeners[path].cache = JSON.parse(event.data)
+                pingDataSubscribed(path)
+                if (currentSubscriptions[path].length === 0) {
+                    delete currentSubscriptions[path]
+                    subscriptionListeners[path].es.close()
+                    delete subscriptionListeners[path]
+                }
+            })
+        }
+
+        console.log("Unfreezing subscriptions")
+        for (const path of Object.keys(frozenSubscriptions)) {
+            const subscriptionGroup = frozenSubscriptions[path]
+            if (Object.keys(currentSubscriptions).indexOf(path) !== - 1) {
+                console.log("Contains " + path)
+                for (const subscription of subscriptionGroup) {
+                    currentSubscriptions[path].add(subscription)
+                }
+
+                addSSEListener(path)
+            } else {
+                console.log("Doesn't contain " + path)
+                currentSubscriptions[path] = subscriptionGroup
+
+                addSSEListener(path)
+            }
+        }
+        frozenSubscriptions = {}
+    }
+})
