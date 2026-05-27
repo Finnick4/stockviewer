1fdb620b012021cba94bab00263671379d5cacf1
gallfeder <finnick4gallfeder@gmail.com>
2026-05-27 17:06:51 +0200

    [F/SETTINGS] Added button to unstar specific starred stocks and stock groups

--- website/script/stockgroups/stockgroups_starred_list.js
+++ website/script/stockgroups/stockgroups_starred_list.js
@@ -24,6 +24,7 @@ class stockgroupsStarredList extends HTMLElement {
             html += `
                 <a class="containing" is="a-button" href="/groups/${e["ID"]}">
                     <div class="name">${sanitiseText(e["Name"])}</div>
+                    <button is="star-stock-group-button" data-stock-group-id="${e.ID}"></button>
                     <div class="value">${getShortNumber(e["TotalValue"]/100)}</div>
                     <div class="value">${getTranslatedStr("stockgroups.member_count", {num: e["MemberCount"]})}</div>
                 </a>
@@ -36,11 +37,19 @@ class stockgroupsStarredList extends HTMLElement {
                             <h2>${getTranslatedStr("stockgroups.starred_list.title")}</h2>
                             <div></div>
                         </div>
-                        <div class="contentTable grid-0-name-2">
+                        <div class="contentTable grid-0-name-3">
                             ${html}
                         </div>
                     </div>
                 `
+
+        that.querySelectorAll("button.star").forEach(btn => {
+            btn.updateStatus(true)
+            btn.addEventListener("click", e => {
+                e.preventDefault()
+                e.stopPropagation()
+            })
+        })
     }
 }
 
--- website/script/stocks/stocklist/starred_delta.js
+++ website/script/stocks/stocklist/starred_delta.js
@@ -47,6 +47,7 @@ class stocklistStarredDelta extends HTMLElement {
                 <a class="containing" is="a-button" href="/stocks/${e["ID"]}" data-stock-id="${e["ID"]}">
                     <div class="shorthand ${Number(e["Color"]) === -1 ? "" : "colored"} ${shouldUseDarkText(getHexColor(Number(e["Color"]))) ? "dark" : "light"}" style="background-color: #${getHexColor(Number(e["Color"]))}">${sanitiseText(e["Shorthand"]).toUpperCase()}</div>
                     <div class="name">${sanitiseText(e["Name"])}</div>
+                    <button is="star-stock-button" data-stock-id="${e.ID}"></button>
                     <div class="value">${shortPrice}</div>
                     <div class="value">${(e["Price2"]-e["Price1"]) >= 0 ? "+" : ""}${getShortNumber((e["Price2"]-e["Price1"]) / 100)}€</div>
                     <div class="value">${getShortNumber((e["Price2"]/e["Price1"] - 1.0)*100)}%</div>
@@ -54,6 +55,13 @@ class stocklistStarredDelta extends HTMLElement {
         })
 
         contentTable.innerHTML = html
+        that.querySelectorAll("button.star").forEach(btn => {
+            btn.updateStatus(true)
+            btn.addEventListener("click", e => {
+                e.preventDefault()
+                e.stopPropagation()
+            })
+        })
     }
 }
 
